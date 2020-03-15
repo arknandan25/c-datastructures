53367e06ff4a7d1430f7c793f4eeebf4692ed812
arknandan25 <arkchauhan10@gmail.com>
2020-03-15 17:07:51 +0000

    leetcode solutions

---
+++ arrays/Two sum-easy-arrays-vectors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> a;
+         a.push_back(0);
+        a.push_back(0);
+        int comp;
+        //use complement approach in this question complement= target-number
+        //unordered_set<int> x;
+        std::vector<int>::iterator k;
+        for(int j=0;j<nums.size();++j){
+            comp = target - nums[j];
+            k = std::find(nums.begin(), nums.end(), comp);
+            if( k != nums.end() && (k-nums.begin())!=j){
+                a[0]=(j);
+                a[1]=(k-nums.begin());
+                return a;
+            }else{
+                continue;
+            }
+          
+        }
+        return a;
+    }
+};
